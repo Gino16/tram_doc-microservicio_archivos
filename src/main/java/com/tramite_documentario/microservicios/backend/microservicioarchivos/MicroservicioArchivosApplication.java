85d3b5c8b42ae4cde47ec13ae73ff932baa37e39
Gino Ascencio <gino.francisco@hotmail.com>
2021-01-12 20:52:01 -0500

    Agregando EntityScan

--- src/main/java/com/tramite_documentario/microservicios/backend/microservicioarchivos/MicroservicioArchivosApplication.java
+++ src/main/java/com/tramite_documentario/microservicios/backend/microservicioarchivos/MicroservicioArchivosApplication.java
@@ -2,12 +2,14 @@ package com.tramite_documentario.microservicios.backend.microservicioarchivos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.tramite_documentario.microservicios.backend.commonarchivos.models.entity"})
 public class MicroservicioArchivosApplication {
 
     public static void main(String[] args) {
