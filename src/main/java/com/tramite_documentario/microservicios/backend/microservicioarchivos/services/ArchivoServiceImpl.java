4e994779972137d5f860517cbcc0e9c236683021
Gino Ascencio <gino.francisco@hotmail.com>
2021-02-24 21:33:25 -0500

    Cambiado mensaje al enviar archivo

--- src/main/java/com/tramite_documentario/microservicios/backend/microservicioarchivos/services/ArchivoServiceImpl.java
+++ src/main/java/com/tramite_documentario/microservicios/backend/microservicioarchivos/services/ArchivoServiceImpl.java
@@ -54,10 +54,10 @@ public class ArchivoServiceImpl extends CommonServiceImpl<Archivo, ArchivoReposi
         helper.setTo(emailsToSend);
         helper.addAttachment(nombreArchivo + ".pdf", documento, "application/json");
 
-        String mensaje = "Envio pdf adjunto de archivo" + nombreArchivo;
+        String mensaje = "Por favor no responder: Envio de archivo" + nombreArchivo;
 
         String contenido = "<h1>Hola Usuario,</h1> " +
-                "<p>Le alcanzo el archivo solicitado</p>";
+                "<p>Se le alcanzo el archivo solicitado</p>";
 
         helper.setSubject(mensaje);
         helper.setText(contenido, true);
